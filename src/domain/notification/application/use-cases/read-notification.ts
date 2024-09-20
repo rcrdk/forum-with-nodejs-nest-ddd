86aed3cdd7a114923f2fcb00956ec1623a1ce220
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-09-20 14:22:13 -0300

    feat: add read notifications

--- src/domain/notification/application/use-cases/read-notification.ts
+++ src/domain/notification/application/use-cases/read-notification.ts
@@ -1,3 +1,5 @@
+import { Injectable } from '@nestjs/common'
+
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { UnauthorizedError } from '@/core/errors/unauthorized-error'
@@ -17,6 +19,7 @@ type ReadNotificationUseCaseResponse = Either<
 	}
 >
 
+@Injectable()
 export class ReadNotificationUseCase {
 	constructor(private notificationsRepository: NotificationsRepository) {}
 
--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,10 +1,13 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
 	Notification,
 	NotificationProps,
 } from '@/domain/notification/enterprise/entities/notification'
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification.mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeNotification(
 	override: Partial<NotificationProps> = {},
@@ -21,4 +24,21 @@ export function makeNotification(
 	)
 
 	return notification
+}
+
+@Injectable()
+export class NotificationFactory {
+	constructor(private prisma: PrismaService) {}
+
+	async makePrismaNotification(
+		data: Partial<NotificationProps> = {},
+	): Promise<Notification> {
+		const question = makeNotification(data)
+
+		await this.prisma.notification.create({
+			data: PrismaNotificationMapper.toPrisma(question),
+		})
+
+		return question
+	}
 }
